87d711fc5db8c6dee5a51559962ca6a9ea3ff4a2
AlinaCalistru <73351416+AlinaCalistru@users.noreply.github.com>
2022-02-19 19:17:42 +0000

    fix href from footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,8 @@ export default function Footer(){
     return(
     <>
     <div className="section footer"> 
-        <a className="social-lin"  onClick={() =>
-              (window.open( "https://www.linkedin.com/in/alina-calistru/", "_blank"))
-            }><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-        <a className="social-git"  target="_blank" onClick={() =>
-              (window.open( "https://bitbucket.org/alinacalistru/", "_blank"))
-            }><FontAwesomeIcon icon={['fab', 'github']} /></a>
+        <a className="social-lin" href="https://www.linkedin.com/in/alina-calistru/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+        <a className="social-git" href="https://www.linkedin.com/in/alina-calistru/" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} /></a>
   
     </div>
     </>
